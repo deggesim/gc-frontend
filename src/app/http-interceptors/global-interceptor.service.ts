d82e34350fcda907df21c14ee645a7cba6714595
deggesim <simone.degennaro@yahoo.it>
2019-01-08 17:17:21 +0100

    Fix interceptor

--- src/app/http-interceptors/global-interceptor.service.ts
+++ src/app/http-interceptors/global-interceptor.service.ts
@@ -1,11 +1,8 @@
-import 'rxjs/add/observable/empty';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/finally';
 
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
+import { empty as observableEmpty, Observable } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
 import { SharedService } from './../shared/shared.service';
 import { SpinnerService } from './../shared/spinner.service';
 
@@ -19,13 +16,13 @@ export class GlobalInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.spinnerService.start();
-    return next.handle(req)
-      .catch((err: HttpErrorResponse) => {
+    return next.handle(req).pipe(
+      catchError((err: HttpErrorResponse) => {
         this.spinnerService.end();
         this.sharedService.notifyError(err);
-        return Observable.empty<HttpEvent<any>>();
-      })
-      .finally(() => this.spinnerService.end());
+        return observableEmpty();
+      }),
+      finalize(() => this.spinnerService.end()));
   }
 
 }
