d40fb441a1e3f07da8fc8d9c36ec00a14f95bc98
Simone De Gennaro <simone.degennaro@yahoo.it>
2021-03-09 17:02:49 +0100

    Removed legacy code

--- src/app/shared/error-page.component.ts
+++ src/app/shared/error-page.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-    moduleId: module.id,
-    selector: 'app-error-page',
-    template: '<h2>Pagina di errore</h2>'
+  selector: "app-error-page",
+  template: "<h2>Pagina di errore</h2>",
 })
-
 export class ErrorPageComponent implements OnInit {
-    constructor() { }
+  constructor() {}
 
-    ngOnInit() { }
+  ngOnInit() {}
 }
